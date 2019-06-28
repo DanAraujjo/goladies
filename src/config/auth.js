bc9f430e062546c75a21729c2b1d19cf0233745b
Daniel Araujo <dan.araujjo@gmail.com>
2019-06-27 22:42:53 -0300

    Autenticação JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8f232c1da0b29a095d2bf3ef19e72363', // gerado em https://www.md5online.org/
+  expiresIn: '1d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserContoller from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserContoller.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
