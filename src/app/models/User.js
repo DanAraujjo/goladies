3f8292d0d8c2ebafbc9f8475b65ec556b97a31b2
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-02 11:01:05 -0300

    Avatar do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,11 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    // n:1
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
