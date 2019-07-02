b2efda3cf8d50a55d910b3f6f5f58cc992165126
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-02 20:56:57 -0300

    Validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -23,7 +24,31 @@ class AppointmentController {
     if (!isProvider) {
       return res
         .status(401)
-        .json({ error: 'Você só pode criar agendamentos com fornecedores.' });
+        .json({ error: 'Você só pode criar agendamento com fornecedores.' });
+    }
+
+    // verificar se a data informada é maior que a data atual
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Não é permitido agendamento para data/hora passada.',
+      });
+    }
+
+    /* Verificar se o horario está disponível */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({
+        error: 'Horário indisponível!',
+      });
     }
 
     const appointment = await Appointment.create({
