77e137f1e10951fab1cf985b90630c348363af3f
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-02 23:33:18 -0300

    Listando agenda do prestador

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,57 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    // verificar se é um fornecedor
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({ error: 'Você não é um fornecedor.' });
+    }
+
+    const { date } = req.query;
+    const { page = 1 } = req.query;
+
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,21 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
-import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
+
 const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
+
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
@@ -28,5 +31,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/appointments', AppointmentController.index);
 
 routes.post('/appointments', AppointmentController.strore);
+
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
