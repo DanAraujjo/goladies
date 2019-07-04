ee505ed0e2533e6d3a56674bd2eab48eae3f8d94
Daniel Araujo <dan.araujjo@gmail.com>
2019-07-04 01:10:43 -0300

    Variáveis ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '8f232c1da0b29a095d2bf3ef19e72363', // gerado em https://www.md5online.org/
+  secret: process.env.APP_SECRET, // gerado em https://www.md5online.org/
   expiresIn: '30d',
 };
